a48796a598fe8b8abe2ea8f87d5a31c74565a368
Vasilii Redinskii <redinskiy@yandex.ru>
2021-03-31 09:44:27 +0300

    Vers.1

--- Exercise_password.py
+++ Exercise_password.py
@@ -1,3 +1,13 @@
 password = input('Введите пароль: ')
+result = 0
+number = 1
 
-print(password)
+try:
+    result = number/len(password)
+    result = int(password)
+    print('Ваш пароль состоит только из цифр')
+except ZeroDivisionError:
+    print('Вы ввели пустой пароль')
+except ValueError:
+    print('Требования к паролю соблюдены')
+ 
